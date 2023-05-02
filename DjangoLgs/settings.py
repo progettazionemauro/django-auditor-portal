cc79b6a3abcfc6db13029e504822039e357f9e41
progettazionemauro <progettazione@studiogb.org>
2023-05-02 19:28:05 +0200

    aggiunta di allowed hosts

--- DjangoLgs/settings.py
+++ DjangoLgs/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@63&#y2242bx9z_^ep8oi2yl(b7^@4hgrs(&_=1avoh90i=-ef
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
